75b09d0e965608922334ed4c440f6b7d6cf79200
Shuremali02 <shuremsyed41@gmail.com>
2024-11-04 13:34:48 -0800

    updated code

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import Header from '../Header/Header'
+import Header from '../Header/Header';
+import { Nunito } from "next/font/google";
+
 
 export default function Banner() {
     return (
@@ -13,7 +15,7 @@ export default function Banner() {
                     <div className='w-[380px] h-[525px]  border border-5 m-5 border-white rounded-tl-[128px] rounded-br-[128px]'></div>
                 </div>
                 <div className='md:w-1/2 flex gap-y-3 p-4 flex-col md:pt-3 justify-center items-center md:gap-y-5 md:items-start'>
-                    <h1 className=' md:h-[162px] font-bold leading-snug  text-3xl md:text-[44px]  '>
+                    <h1 className=' md:h-[162px] font-serif font-bold leading-snug  text-3xl md:text-[44px]  '>
                         IMPECCABLE CRAFTSMANSHIP AND FINESSE
                     </h1>
                     <p className='md:w-[798px] md:h-36 md:text-[29px] mb-2 font-medium text-md  text-[#787054]'>
@@ -25,17 +27,11 @@ export default function Banner() {
                 <div className='w-[421px] h-[573px] bg-cover bg-center hidden md:flex justify-center items-center rounded-tl-[128px] rounded-br-[128px]'
                     style={{ backgroundImage: `url('/Images/banner_2.png')` }}
                 >
-                    <div className='w-[380px] h-[525px]  border border-5 m-5 border-white rounded-tl-[128px] rounded-br-[128px]'></div>
+                    <div className='w-[380px] h-[525px]  border border-5 m-5 border-white rounded-tl-[128px] rounded-br-[128px]'>
 
+                    </div>
                 </div>
-
-
             </div>
-
-
-
         </div>
-
-
     )
 }
